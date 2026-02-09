5345cd311d5240b7c2451f9a50a1cec8d4735fc2
DevashishParas <devashish.web@gmail.com>
2026-02-09 23:15:51 +0530

    Notes

--- pattern1.cpp
+++ pattern1.cpp
@@ -10,3 +10,5 @@ int main(){
     }
     return 0;
 }
+
+https://github.com/DevashishParas/DSA
--- pattern6.cpp
+++ pattern6.cpp
@@ -5,7 +5,7 @@ int main(){
     cin>>n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i-1;j++){
-            cout<<"  ";
+            cout<<"    ";
         }
             for(int k=1;k<=n-i+1;k++){
                 cout<<"* ";
